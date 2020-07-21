db5b3d10672178a0e12d0587ade3ced8a0af7d07
Sitifatmh <sf4757401@gmail.com>
2020-07-21 20:19:36 +0700

    ebook-api

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.main');
+
+@section('title', 'about')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+        <h1 class="mt-2">saya adalah siti fatimah</h1>
+        </div>
+    </div>
+    @endsection
