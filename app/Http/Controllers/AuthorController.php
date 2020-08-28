6ffecfb9d7b90c78fc727df6f80a29906fd2027e
Sitifatmh <sf4757401@gmail.com>
2020-08-28 13:52:29 +0700

    challenge-author

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Book;
+use App\Author;
 
-class BookController extends Controller
+class AuthorController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,10 +14,10 @@ class BookController extends Controller
      */
     public function index()
     {
-        // native: select * from books;
-        $book = Book::all();
-        if($book && $book->count() > 0){
-            return response(['message'=> 'Show data success.', 'data'=> $book], 200);
+        //
+        $author = Author::all();
+        if($author && $author->count() > 0){
+            return response(['message'=> 'Show data success.', 'data'=> $author], 200);
         }else{
             return response(['message'=> 'Data not found.', 'data'=> null], 404);
         }
@@ -32,14 +32,15 @@ class BookController extends Controller
     public function store(Request $request)
     {
         //
-        $book = Book::create([
-            "title" => $request->title,
-            "description" => $request->description,
-            "author" => $request->author,
-            "publisher" => $request->publisher,
-            "date_of_issue" => $request->date_of_issue
-        ]);
-        return response(['message'=> 'Create data success.', 'data'=> $book], 201);
+        $author = Author::create ([
+            "name" => $request->name,
+            "date_of_birth" => $request->date_of_birth,
+            "place_of_birth" => $request->place_of_birth,
+            "gender" => $request->gender,
+            "email" => $request->email,
+            "hp" => $request->hp
+     ]);
+        return response(['message'=> 'Create data success.', 'data'=> $author], 201);    
     }
 
     /**
@@ -50,10 +51,10 @@ class BookController extends Controller
      */
     public function show($id)
     {
-        //nataive: select * from books where id = 1
-        $book = Book::find($id);
-        if($book && $book->count() > 0){
-            return response(['message'=> 'Show data success.', 'data'=> $book], 200);
+        //
+        $author = Author::find($id);
+        if($author && $author->count() > 0){
+            return response(['message'=> 'Show data success.', 'data'=> $author], 200);
         }else{
             return response(['message'=> 'Data not found.', 'data'=> null], 404);
         }
@@ -69,15 +70,16 @@ class BookController extends Controller
     public function update(Request $request, $id)
     {
         //
-        $book = Book::find($id);
-        if($book){
-            $book ->title = $request->title;
-            $book ->description = $request->description;
-            $book ->author = $request->author;
-            $book ->publisher = $request->publisher;
-            $book ->date_of_issue = $request->date_of_issue;
-            $book->save();
-            return response(['message'=> 'Update data success.', 'data'=> $book], 200);
+        $author = Author::find($id);
+        if($author){
+            $author ->name = $request->name;
+            $author ->date_of_birth = $request->date_of_birth;
+            $author ->place_of_birth = $request->place_of_birth;
+            $author ->gender = $request->gender;
+            $author ->email = $request->email;
+            $author ->hp = $request->hp;
+            $author->save();
+            return response(['message'=> 'Update data success.', 'data'=> $author], 200);
         }else{
             return response(['message'=> 'Update data failed.', 'data'=> null], 406);
         }
@@ -91,14 +93,13 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        //
-        $book =  Book::find($id);
-        if($book){
-            $book->delete();
+        $author = Author::find($id);
+        if($author){
+            $author->delete();
             return response([], 204);
         }else{
             return response(['message'=> 'Remove data failed.', 'data'=> null], 406);
             
         }
-    }
+    }    
 }
